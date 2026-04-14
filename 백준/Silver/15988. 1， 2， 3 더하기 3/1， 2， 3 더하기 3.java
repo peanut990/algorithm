b5322827a6cca54484d078d0caa5fec0dbdd5e21
peanut990 <112597929+peanut990@users.noreply.github.com>
2026-04-14 17:06:12 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 212 ms, Memory: 25836 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        long[] dp = new long[1_000_001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 1_000_000; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1_000_000_009;
+        }
+
+        for (int t = 0; t < T; t++) {
+            int n = sc.nextInt();
+            System.out.println(dp[n]);
+        }
+    }
+}
