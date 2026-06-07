c9a958cb232737008e6be6c8a0260c5fc34186e9
peanut990 <112597929+peanut990@users.noreply.github.com>
2026-06-08 00:35:05 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.29 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[n+3];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3;i<=n;i++){
+            dp[i] = (dp[i-1] + dp[i-2])%1_000_000_007;
+        }
+        return dp[n];
+    }
+}
+/*
+11
+-
+-
+
+n= 2 /2
+n= 3 /2 + 1
+
+
+*/
