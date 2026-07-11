d191d835b9bc6fceb94673af1964792b639ed208
peanut990 <112597929+peanut990@users.noreply.github.com>
2026-07-11 21:18:08 +0900

    [level 2] Title: 점 찍기, Time: 5.10 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for(int x = 0;x<=d;x+= k){
+            long y = (long)Math.sqrt((long)d*d - (long)x*x);
+            answer += (y/k) + 1;
+        }
+        
+        return answer;
+    }
+}
